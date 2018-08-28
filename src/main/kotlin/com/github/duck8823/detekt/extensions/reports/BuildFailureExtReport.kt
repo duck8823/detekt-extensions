753a327d06d45df41965f51581f28741af7895f1
shunsuke maeda <duck8823@gmail.com>
2018-08-28 21:06:35 +0900

    Implementation sample code

--- src/main/kotlin/com/github/duck8823/detekt/extensions/reports/BuildFailureExtReport.kt
+++ src/main/kotlin/com/github/duck8823/detekt/extensions/reports/BuildFailureExtReport.kt
@@ -1,11 +1,19 @@
 package com.github.duck8823.detekt.extensions.reports
 
+
+import io.gitlab.arturbosch.detekt.core.processors.logicalLinesKey
 import io.gitlab.arturbosch.detekt.api.ConsoleReport
 import io.gitlab.arturbosch.detekt.api.Detektion
 
 class BuildFailureExtReport : ConsoleReport() {
 
     override fun render(detektion: Detektion): String? {
-        return "Hello Report"
+        val lloc = detektion.getData(logicalLinesKey)
+        val smells = detektion.findings.entries.sumBy { it.value.size }
+        val dirty = smells * 1000 / (lloc as Int)
+        if ( dirty >= 10 ) {
+            throw RuntimeException("dirty: $dirty smells per 1,000 lloc")
+        }
+        return null
     }
 }
