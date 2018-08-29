21af8bede13e7371220b0b2ca3c7b8350748a0f4
shunsuke maeda <duck8823@gmail.com>
2018-08-29 13:06:17 +0900

    Implementation code

--- src/main/kotlin/com/github/duck8823/detekt/extensions/reports/BuildFailureExtReport.kt
+++ src/main/kotlin/com/github/duck8823/detekt/extensions/reports/BuildFailureExtReport.kt
@@ -1,40 +1,82 @@
 package com.github.duck8823.detekt.extensions.reports
 
-
-import io.gitlab.arturbosch.detekt.api.ConsoleReport
-import io.gitlab.arturbosch.detekt.api.Detektion
-import io.gitlab.arturbosch.detekt.api.PREFIX
+import io.gitlab.arturbosch.detekt.api.*
 import io.gitlab.arturbosch.detekt.core.processors.*
 
 class BuildFailureExtReport : ConsoleReport() {
 
+    override val priority: Int = Int.MIN_VALUE
+
+    private var complexityThresholds: Config by SingleAssign()
+    private var thresholdLoc: Int by SingleAssign()
+    private var thresholdSloc: Int by SingleAssign()
+    private var thresholdLloc: Int by SingleAssign()
+    private var thresholdCloc: Int by SingleAssign()
+    private var thresholdMcc: Int by SingleAssign()
+    private var thresholdSmells: Int by SingleAssign()
+    private var thresholdMccPerThousandLLOC: Int by SingleAssign()
+    private var thresholdSmellsPerThousandLLOC: Int by SingleAssign()
+
+    companion object {
+        private const val COMPLEXITY_THRESHOLDS = "complexityThresholds"
+        private const val LOC = "loc"
+        private const val SLOC = "sloc"
+        private const val LLOC = "lloc"
+        private const val CLOC = "cloc"
+        private const val MCC = "mcc"
+        private const val SMELLS = "smells"
+        private const val MCC_PER_THOUSAND_LLOC = "mccPerThousandLLOC"
+        private const val SMELLS_PER_THOUSAND_LLOC = "llocPertThousandLLOC"
+    }
+
+    override fun init(config: Config) {
+        complexityThresholds = config.subConfig(COMPLEXITY_THRESHOLDS)
+        thresholdLoc = complexityThresholds.valueOrDefault(LOC, Int.MAX_VALUE)
+        thresholdSloc = complexityThresholds.valueOrDefault(SLOC, Int.MAX_VALUE)
+        thresholdLloc = complexityThresholds.valueOrDefault(LLOC, Int.MAX_VALUE)
+        thresholdCloc = complexityThresholds.valueOrDefault(CLOC, Int.MAX_VALUE)
+        thresholdMcc = complexityThresholds.valueOrDefault(MCC, Int.MAX_VALUE)
+        thresholdSmells = complexityThresholds.valueOrDefault(SMELLS, Int.MAX_VALUE)
+        thresholdMccPerThousandLLOC =
+                complexityThresholds.valueOrDefault(MCC_PER_THOUSAND_LLOC, Int.MAX_VALUE)
+        thresholdSmellsPerThousandLLOC =
+                complexityThresholds.valueOrDefault(SMELLS_PER_THOUSAND_LLOC, Int.MAX_VALUE)
+    }
+
     override fun render(detektion: Detektion): String? {
-        val mcc = detektion.getData(complexityKey) as Int
         val loc = detektion.getData(linesKey) as Int
         val sloc = detektion.getData(sourceLinesKey) as Int
         val lloc = detektion.getData(logicalLinesKey) as Int
         val cloc = detektion.getData(commentLinesKey) as Int
-        val findings = detektion.findings.entries
-
-        val numberOfSmells = findings.sumBy { it.value.size }
-        val smellPerThousandLines = numberOfSmells * 1000 / lloc
+        val mcc = detektion.getData(complexityKey) as Int
+        val smells = detektion.findings.entries.sumBy { it.value.size }
         val mccPerThousandLines = mcc * 1000 / lloc
-        val commentSourceRatio = cloc * 100 / sloc
+        val smellsPerThousandLines = smells * 1000 / lloc
 
-        return with(StringBuilder()) {
-            append(("Complexity Report:".format()))
-            append("$loc lines of code (loc)".format(PREFIX))
-            append("$sloc source lines of code (sloc)".format(PREFIX))
-            append("$lloc logical lines of code (lloc)".format(PREFIX))
-            append("$cloc comment lines of code (cloc)".format(PREFIX))
-            append("$mcc McCabe complexity (mcc)".format(PREFIX))
-            append("$numberOfSmells number of total code smells".format(PREFIX))
-            append("$commentSourceRatio % comment source ratio".format(PREFIX))
-            append("$mccPerThousandLines mcc per 1000 lloc".format(PREFIX))
-            append("$smellPerThousandLines code smells per 1000 lloc".format(PREFIX))
-            toString()
+        return when {
+            thresholdLoc.reached(loc) -> throw BuildFailure("Build failure threshold of " +
+                    "$thresholdLoc reached with $loc line of code!")
+            thresholdSloc.reached(sloc) -> throw BuildFailure("Build failure threshold of " +
+                    "$thresholdSloc reached with $sloc source lines of cod!")
+            thresholdLloc.reached(lloc) -> throw BuildFailure("Build failure threshold of " +
+                    "$thresholdLloc reached with $lloc logical lines of code!")
+            thresholdCloc.reached(cloc) -> throw BuildFailure("Build failure threshold of " +
+                    "$thresholdCloc reached with $cloc comment lines of code!")
+            thresholdMcc.reached(mcc) -> throw BuildFailure("Build failure threshold of " +
+                    "$thresholdMcc reached with $mcc McCabe complexity!")
+            thresholdSmells.reached(smells) -> throw BuildFailure("Build failure threshold of " +
+                    "$thresholdSmells reached with $loc number of total code smells!")
+            thresholdMccPerThousandLLOC.reached(mccPerThousandLines) ->
+                throw BuildFailure("Build failure threshold of $thresholdMccPerThousandLLOC  " +
+                        "reached with $mccPerThousandLines mcc per 1000 lloc!")
+            thresholdSmellsPerThousandLLOC.reached(smellsPerThousandLines) ->
+                throw BuildFailure("Build failure threshold of $thresholdSmellsPerThousandLLOC " +
+                        "reached with $smellsPerThousandLines smells per 1000 lloc!")
+            else -> null
         }
     }
 
-    private fun Any.format(prefix: String = "", suffix: String = "\n") = "$prefix$this$suffix"
+    private fun Int.reached(amount: Int): Boolean = !(this == 0 && amount == 0) && this != -1 && this <= amount
+
+    class BuildFailure(message: String) : RuntimeException(message)
 }
