041000781683cecbee5acbebe9d64c1e88926826
shunsuke maeda <duck8823@gmail.com>
2018-11-12 00:00:22 +0900

    Add custom rule

---
+++ src/main/kotlin/com/github/duck8823/detekt/extensions/rules/RequireBracesIfStatements.kt
@@ -0,0 +1,33 @@
+package com.github.duck8823.detekt.extensions.rules
+
+import io.gitlab.arturbosch.detekt.api.*
+import org.jetbrains.kotlin.psi.KtBlockExpression
+import org.jetbrains.kotlin.psi.KtIfExpression
+
+class RequireBracesIfStatements(config: Config = Config.empty) : Rule(config) {
+        override val issue: Issue = Issue(
+                id = RequireBracesIfStatements::class.java.simpleName!!,
+                severity = Severity.Style,
+                description = """\
+                    |If statement was found that does not have braces. \
+                    |These should be added to improve readability.""".trimMargin(),
+                debt = Debt.FIVE_MINS
+        )
+
+        override fun visitIfExpression(expression: KtIfExpression) {
+                if (expression.isNotBlockExpression()) {
+                        report(CodeSmell(
+                                issue = issue,
+                                entity = Entity.from(expression),
+                                message = """\
+                                    |If statement was found that does not have braces. \
+                                    |These should be added to improve readability.""".trimMargin()
+                        ))
+                }
+                super.visitIfExpression(expression)
+        }
+
+        private fun KtIfExpression.isNotBlockExpression(): Boolean =
+                this.then !is KtBlockExpression
+
+}
